1472ef460afcb861df2855540f385f1b2d1d0a6c
Pevita-PC\Pevita <ajienata13@gmail.com>
2016-12-08 14:51:38 +0700

    perubahan font

--- ONLINE STORE/ONLINE STORE/form selamat datang.Designer.vb
+++ ONLINE STORE/ONLINE STORE/form selamat datang.Designer.vb
@@ -30,7 +30,7 @@ Partial Class form_selamat_datang
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(0, -1)
+        Me.Label1.Location = New System.Drawing.Point(16, 5)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(197, 37)
         Me.Label1.TabIndex = 0
@@ -38,22 +38,23 @@ Partial Class form_selamat_datang
         '
         'Button1
         '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(76, 39)
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(89, 45)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(39, 29)
+        Me.Button1.Size = New System.Drawing.Size(45, 33)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "OK"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'form_selamat_datang
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(193, 75)
+        Me.ClientSize = New System.Drawing.Size(225, 87)
         Me.ControlBox = False
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "form_selamat_datang"
         Me.ResumeLayout(False)
--- ONLINE STORE/ONLINE STORE/form selamat datang.vb
+++ ONLINE STORE/ONLINE STORE/form selamat datang.vb
@@ -1,3 +1,6 @@
 ﻿Public Class form_selamat_datang
 
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
