0b9ec98c711ea09dcacb21ba8e77a47a631321c1
Pevita-PC\Pevita <ajienata13@gmail.com>
2016-12-08 19:36:22 +0700

    ASA

--- ONLINE STORE/ONLINE STORE/form selamat datang.vb
+++ ONLINE STORE/ONLINE STORE/form selamat datang.vb
@@ -3,4 +3,8 @@
     Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+    End Sub
 End Class
