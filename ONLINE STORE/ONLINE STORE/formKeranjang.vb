6ecdb28d7049345261ea3fac335a0dd6e8d16ee5
ryairawan-PC\rya irawan <nrriairawan2330@gmail.com>
2016-12-18 14:33:59 +0800

    perbaikan dan penambahan di form keranjang

--- ONLINE STORE/ONLINE STORE/formKeranjang.vb
+++ ONLINE STORE/ONLINE STORE/formKeranjang.vb
@@ -20,8 +20,4 @@
     'Me.PembelianTableAdapter.Fill(Me.OnlinesotreDataSet.pembelian)
 
     'End Sub
-
-    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
-
-    End Sub
 End Class
